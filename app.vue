8e1782df1520db25018bc8b5063fe6ad069ac91f
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-21 08:32:44 +0200

    wip

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { attributes } from '@/content/file.md'
+
+console.log(attributes) //=> { title: 'Awesome Title', description: 'Describe this awesome content', tags: ['great', 'awesome', 'rad'] }
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 import { defineNuxtConfig } from 'nuxt'
+import Vue from '@vitejs/plugin-vue'
+import AntfuMarkdown from 'vite-plugin-md'
+const mdPlugin = require('vite-plugin-markdown')
+import markdown from '@dansvel/vite-plugin-markdown'
+import { vitePluginMdToHTML } from 'vite-plugin-md-to-html'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    plugins: [
+      // Vue({
+      // include: [/\.vue$/, /\.md$/],
+      // }),
+      // Markdown(),
+      // mdPlugin({}),
+      markdown({}),
+      // vitePluginMdToHTML()
+    ],
+  },
+  modules: ['@nuxtjs/tailwindcss'],
 })
