112a73085c8f3aee01bf6bc65a66e1f9b811d044
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-06-25 08:17:31 +0200

    svg icon fix

--- components/svg-icon.vue
+++ components/svg-icon.vue
@@ -5,7 +5,9 @@ const props = defineProps<{
 
 const currentIcon = computed(() =>
   defineAsyncComponent({
-    loader: () => import(`../assets/icons/${props.name}.svg`),
+    // loader: () => import(`../assets/icons/${props.name}.svg`),
+    // loading: https://github.com/jpkleemans/vite-svg-loader/issues/47
+    loader: () => import('../assets/' + props.name + '.svg?component'),
     loadingComponent: {
       template: '<span></span>'
     },
@@ -18,28 +20,16 @@ const currentIcon = computed(() =>
   })
 )
 const attrs = useAttrs()
+
+const defaultSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="${attrs.class}"><path stroke-linecap="round" stroke-linejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" /></svg>`
 </script>
 
 <template>
   <span>
     <client-only>
       <component :is="currentIcon" :class="attrs.class" />
-      <!-- <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-        :class="attrs.class"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-        />
-      </svg> -->
       <template #fallback>
-        <span class="sr-only" />
+        <span v-html="defaultSvg"></span>
       </template>
     </client-only>
   </span>
