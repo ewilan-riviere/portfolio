47720218fac32f6e98098a2bee1729462b0270bc
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-07-07 11:08:02 +0200

    svg, contact, translate

--- components/app/social.vue
+++ components/app/social.vue
@@ -13,11 +13,11 @@ const socialItems = about.socialItems
       :href="item.link"
       target="_blank"
       rel="noopener noreferrer"
-      :title="item.title"
+      :title="item.name"
       class="text-gray-800 dark:text-gray-200 hover:bg-gray-300 hover:bg-opacity-30 transition-colors duration-100 p-2 rounded-md block"
     >
       <span class="sr-only">
-        {{ item.title }}
+        {{ item.name }}
       </span>
       <svg-icon :name="`social-${item.slug}`" class="w-6 h-6" />
     </a>
