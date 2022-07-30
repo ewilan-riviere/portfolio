8b941b92b15d30c45169d99e910bb8649cec842d
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-07-30 13:58:01 +0200

    ⚡️ clean

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-    <div>
-      <nuxt-link to="/">
-        home
-      </nuxt-link>
-      <nuxt-link to="/about">
-        about
-      </nuxt-link>
-    </div>
     <slot />
   </div>
 </template>
