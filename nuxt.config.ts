7b0edb53183f3ac7b82671cbd011abac9f5aea79
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-11-27 17:49:37 +0100

    clean

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,8 @@ export default defineNuxtConfig({
   css: ['~/assets/css/app.css'],
   modules: [
     '@nuxt/content', // https://content.nuxtjs.org/get-started,
-    'nuxt-ray',
     'nuxt-svg-transformer',
   ],
-  // ray: {
-  //   port: 23517,
-  // },
-  svgTransformer: {
-  },
   // https://content.nuxtjs.org/api/configuration
   content: {
     documentDriven: true,
