0b3ccb854b978f8ac7c736ebecf5c3f095faa392
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-04-27 09:42:44 +0200

    fix debug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const isDev = import.meta.env.DEV
+</script>
 
 <template>
-  <div class="flex h-full flex-col bg-zinc-50 dark:bg-black debug-screens">
+  <div
+    :class="{
+      'debug-screens': isDev,
+    }"
+    class="flex h-full flex-col bg-zinc-50 dark:bg-black"
+  >
     <div class="fixed inset-0 flex justify-center sm:px-8">
       <div class="flex w-full max-w-7xl lg:px-8">
         <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
