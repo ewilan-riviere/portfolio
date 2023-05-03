ae54257b8d5cf65d2917d1ca36186847ebea67c8
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-05-03 12:14:49 +0200

    gallery add links

--- components/home/gallery.vue
+++ components/home/gallery.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { Project } from '~/types/project'
+
 defineProps<{
   projects: Project[]
 }>()
@@ -15,21 +17,25 @@ projects = shuffle(projects)
 <template>
   <div class="mt-16 sm:mt-20">
     <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-      <div
-        v-for="(project, index) in projects" :key="project.slug"
+      <TypedLink
+        v-for="(project, index) in projects"
+        :key="project.slug"
+        :to="{
+          name: 'projects-slug',
+          params: {
+            slug: project.slug,
+          },
+        }"
         :class="index % 2 === 0 ? 'rotate-2' : '-rotate-2'"
-        class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl border border-zinc-100 dark:border-zinc-700"
+        class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl border border-zinc-100 dark:border-zinc-700 block"
       >
-        <!-- <img alt="" sizes="(min-width: 640px) 18rem, 11rem" decoding="async" data-nimg="1" class="absolute inset-0 h-full w-full object-cover" loading="lazy" style="color:transparent" width="3744" height="5616"> -->
         <div class="absolute inset-0 h-full w-full">
           <app-img
             :src="project.image" :alt="project.title"
             class="h-full w-full object-cover"
           />
         </div>
-      </div>
+      </TypedLink>
     </div>
   </div>
 </template>
-
-<style scoped></style>
